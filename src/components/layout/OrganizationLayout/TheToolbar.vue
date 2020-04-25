c510596dab2229cd35adede02c80fda2f8adb9e0
Kami <1.dankon.1@gmail.com>
2020-04-25 14:47:12 +0300

    Добавтл layout организации

---
+++ src/components/layout/OrganizationLayout/TheToolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar color="primary" clipped-left dark app>
+    <div>
+      <v-img src="/favicon.ico" max-width="230" class="ml-1" contain />
+    </div>
+    <v-spacer />
+
+    <v-toolbar-items>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+/**
+ * Компонент верхнего меню
+ */
+
+export default {
+  name: "TheToolbar",
+};
+</script>
---
+++ src/components/layout/OrganizationLayout/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-content>
+    <the-toolbar />
+
+    <v-container fluid class="px-0 mt-4">
+      <v-layout fill-height justify-center>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-layout>
+    </v-container>
+
+    <the-footer />
+  </v-content>
+</template>
+
+<script>
+import TheToolbar from './TheToolbar';
+import TheFooter from './TheFooter';
+
+export default {
+  name: 'OrganizationLayout',
+
+  components: {
+    TheToolbar,
+    TheFooter,
+  },
+};
+</script>
