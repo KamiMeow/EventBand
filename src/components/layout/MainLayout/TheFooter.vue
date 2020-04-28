eff56df79250251b3b444bcee064d9e0a7eb1036
Дима Бурмистров <supamem74@gmail.com>
2020-04-28 05:02:53 +0300

    MainPage ui 100%

--- src/components/layout/MainLayout/TheFooter.vue
+++ src/components/layout/MainLayout/TheFooter.vue
@@ -1,12 +1,34 @@
 <template>
   <v-footer
+		class="py-10"
     color="primary"
     dark
     app
   >
-    <span class="px-3 body-2" aria-label="Написать в поддержку" data-direction="top">
-      &copy; {{ new Date().getFullYear() }} — all rigths reserved
-    </span>
+		<v-col
+			cols="3">
+			<span 
+				class="px-3 body-2" aria-label="Написать в поддержку" data-direction="top">
+				&copy; {{ new Date().getFullYear() }} — all rigths reserved
+			</span>
+		</v-col>
+		<v-col
+			cols="6"
+			class="d-flex flex-row justify-center align-center">
+			<v-img
+				v-for="icn in icons"
+				:key="icn"
+				:src="pathToIcons + icn + format"
+				class="ma-1"
+				max-width="32"
+				max-height="32"
+			></v-img>
+		</v-col>
+		<v-col
+			cols="3"
+			align="right">
+			Privacy | Conditions	
+		</v-col>
   </v-footer>
 </template>
 
@@ -15,7 +37,18 @@
  * Компонент футера
  */
 
+
 export default {
-  name: 'TheFooter',
+	name: 'TheFooter',
+	data() {
+		return {
+			pathToIcons: "/socmedia-icons/socmedia-",
+			format: ".png",
+			icons: ['1', '2', '3', '4'],
+		}
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,26 +1,76 @@
 <template>
   <v-img
     src="/blur-bg-main.png"
-    style="height: 90vh;"
-		class="d-flex justify-center align-center"
+    style="height: 90%"
   >
-		<v-layout
-			style="width: 100%;"
-			height="100"
-			color="white"
+		<v-container
+			class="d-flex justify-space-around align-center"
+			style="height: 90%"
+			fluid
 		>
-			
-		</v-layout>
+			<v-card
+				v-for="offer in offers"
+				:key="offer.icon"
+				class="py-5 px-1 semi-opacity"
+				width="350"
+				outlined
+				tile
+			>
+				<div class="d-flex flex-column align-center white--text">
+					<v-img
+						:src="'/'+ offer.icon +'-icon.'+ offer.format"
+					></v-img>
+					<v-card-title class="headline"> {{ offer.title }} </v-card-title>
+					<v-card-text align="center" class="title">
+						{{ offer.text }}
+					</v-card-text>
+				</div>
+			</v-card>			
+		</v-container>
+		
   </v-img>
 </template>
 
 <script>
 
+const offers = [
+	{
+		icon: 'search',
+		format: 'png',
+		title: 'Search events',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla rerum animi numquam voluptate velit ad veniam earum vitae reiciendis.',
+	},
+	{
+		icon: 'calendar',
+		format: 'png',
+		title: 'Subscribe on events',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla rerum animi numquam voluptate velit ad veniam earum vitae reiciendis.',
+	},
+	{
+		icon: 'chat',
+		format: 'png',
+		title: 'Intercourse',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla rerum animi numquam voluptate velit ad veniam earum vitae reiciendis.',
+	},
+];
+
+
 export default {
-  name: 'MainPage',
+	name: 'MainPage',
+	data() {
+		return {
+			offers,
+		}
+	},
 }
 </script>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
+	.semi-opacity {
+		background-color: rgba($color: #FFFFFF, $alpha: 0.25) !important;
+   	border-color: white !important;
+		border-width: 2px !important;
+		border-radius: 0px !important;
+	}
 </style>
