2d1a9c8e155795f40e5e5ce72450d0a975556822
Kami <1.dankon.1@gmail.com>
2020-05-22 00:29:19 +0300

    create private routes

--- src/router/listners/beforeEach.js
+++ src/router/listners/beforeEach.js
@@ -4,27 +4,32 @@ const NOT_LOGIN_META = 'notLogin';
 const NEED_LOGIN_META = 'needLogin';
 const FOR_ALL_META = 'all';
 
+const lastRoute = {};
+
+function isEmpty(object) {
+  return !Object.keys(object).length;
+};
+
 export default function (store) {
   return (to, _, next) => {
-    const isSigned = store.getters['auth/getIsSigned'];
+    const isLogged = store.getters['auth/getIsLogged'];
     const token = store.getters['auth/getToken'];
-
-    const { access } = to.meta;
 
     if (token) {
       webClient.login(token);
     }
 
-    if (!access || access === FOR_ALL_META) next();
-    if (isSigned) {
-      if (access === NEED_LOGIN_META) next();
-      else if (access === NOT_LOGIN_META) next('/');
+    const isPing = !!(to.path === '/ping' && lastRoute);
+    const { meta:   { access }, path } = isPing ? lastRoute : to;
+
+    if ((isEmpty(lastRoute) && to.name === 'sign-in') || to.name !== 'sign-in') Object.assign(lastRoute, to);
+    if (!isLogged && access === NEED_LOGIN_META) {
+      return next('/login');
     }
-    else {
-      if (access === NEED_LOGIN_META) next('/sign-in');
-      else if (access === NOT_LOGIN_META) next();
+    else if (isLogged && access === NOT_LOGIN_META) {
+      return next('/');
     }
 
-    next();
+    return next((isPing && path) || true);
   };
 };
