79198864ed77672398c04dbc6d8445cb6204e7c7
Kami <1.dankon.1@gmail.com>
2020-05-21 22:50:53 +0300

    Обработка перехода на запрещённые пути

--- src/router/listners/beforeEach.js
+++ src/router/listners/beforeEach.js
@@ -1,10 +1,36 @@
-/**
- * Хук роутера, вызывается перед переходом на страницу 
- * @param {VueX.Store} store Хранилище приложения
- */
+import webClient from '@/middleware/WebClient';
+
+const NOT_LOGIN_META = 'notLogin';
+const NEED_LOGIN_META = 'needLogin';
+const FOR_ALL_META = 'all';
+
+const lastRoute = {};
+function isEmpty(object) {
+  return !Object.keys(object).length;
+};
+
 export default function (store) {
   return (to, _, next) => {
-		let isAuth = store.getters['auth/getIsLogged'];	
+    const isSigned = store.getters['auth/getIsSigned'];
+    const token = store.getters['auth/getToken'];
+
+    const { access } = to.meta;
+
+    if (token) {
+      webClient.login(token);
+    }
+
+    console.log(to.meta, isSigned, access);
+    if (!access || access === FOR_ALL_META) next();
+    if (isSigned) {
+      if (access === NEED_LOGIN_META) next();
+      else if (access === NOT_LOGIN_META) next('/');
+    }
+    else {
+      if (access === NEED_LOGIN_META) next('/sign-in');
+      else if (access === NOT_LOGIN_META) next();
+    }
+
     next();
   };
 };
