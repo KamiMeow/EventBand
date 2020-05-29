169885de91e454f31ffa33006e90da47f9ab48bb
Дима Бурмистров <supamem74@gmail.com>
2020-05-29 19:26:20 +0300

    Config| CORS Resolved

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+	preset: '@vue/cli-plugin-unit-jest',
+	setupFiles: [ 'D:/EventBand/EventBand/tests/unit/index.js' ],
+	testPathIgnorePatterns: [
+    '/build/',
+    '/config/',
+    '/data/',
+    '/dist/',
+    '/node_modules/',
+    '/test/',
+    '/vendor/'
+  ],
+	globals: {
+    NODE_ENV: 'test'
+  },
 }
