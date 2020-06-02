75d797ff7fa3812519f579539c99f815927ebe50
Дима Бурмистров <supamem74@gmail.com>
2020-06-02 21:15:22 +0300

    OrganizationService: commented

--- src/middleware/services/OrganizationService.js
+++ src/middleware/services/OrganizationService.js
@@ -1,5 +1,14 @@
 
 export default {
+	
+	/**
+	 * Посылает запрос на создание организации
+	 * @param {Object} Destructured - Деструктурированный параметр
+	 * @param {String} Destructured.name - Название организации
+	 * @param {String} Destructured.description  - Описание организации
+	 * @param {File} Destructured.logo - Логотип организации
+	 * @returns {Promise}
+	 */
 	createOrganization( {name, description, logo} ) {
 		let formData = new FormData();
 		formData.append('name', name);
@@ -8,6 +17,11 @@ export default {
 		return this.apiClient.post('organization/create', formData);
 	},
 
+	/**
+	 * Послыает запрос на авторизацию в роли Организации
+	 * @param {String} uuid - UUID организации
+	 * @returns {Promise}
+	 */
 	async signInAsOrganization( uuid ) {
 		let response = await this.apiClient.get(`organization/login/${uuid}`);
 		let errorMessage = {
@@ -27,22 +41,49 @@ export default {
 		return response;
 	},
 
-	getMyOrganizationInfo( ) {
+	/**
+	 * Послыает запрос на получение всей информации об организации пользователя
+	 * @returns {Promise}
+	 */
+	getMyOrganizationInfo() {
 		return this.apiClient.get(`my-organization/`);
 	},
 
+	/**
+	 * Посылает запрос на добавление пользователя в организацию в качестве администратора
+	 * @param {String} email - Почта пользователя
+	 * @returns {Promise}
+	 */
 	sendInviteBeOrganizer( email ) {
 		return this.apiClient.post('my-organization/organizers/request', { email });
 	},
 
+	/**
+	 * Посылает запрос на создание мероприятия
+	 * @param {Object} news - Объект с необходимыми данными для создания мероприятия
+	 * @param {String} news.title - Заголовок новости
+	 * @param {String} news.text - Текст новости
+	 * @returns {Promise}
+	 */
 	createNews( news ) {
 		return this.apiClient.post('my-organization/news/create', { ...news });
 	},
 
+	/**
+	 * Посылает запрос на получение всех новостей организации пользователя
+	 * @returns {Promise}
+	 */
 	getMyNews() {
 		return this.apiClient.get('my-organization/news/all');
 	},
 
+	/**
+	 * Посылает запрос на изменение информации об организации
+	 * @param {Object} info 
+	 * @param {String} info.name - Название организации 
+	 * @param {String} info.description - Описание организации 
+	 * @returns {Promise}
+	 */
 	async updateOrganizationInfo( info ) {
 		return this.apiClient.put('my-organization/', { ...info });
 	},
