68298d04f9ef1dcd17df4272e96572a3a7e9c64c
Дима Бурмистров <supamem74@gmail.com>
2020-06-02 21:52:59 +0300

    UserService: commented

--- src/middleware/services/UserService.js
+++ src/middleware/services/UserService.js
@@ -1,6 +1,13 @@
 const url = "users";
 
 export default {
+
+	/**
+	 * Посылает запрос на авторизацию
+	 * @param {String} email - Почта
+	 * @param {String} password - Пароль
+	 * @returns {Promise}
+	 */
 	async signIn( email, password ) {
 		let response = (await this.apiClient.post('login', { email, password })).data;
 				
@@ -10,6 +17,16 @@ export default {
 		return response;
 	},
 
+	/**
+	 * Посылает запрос на регистрацию пользователя
+	 * @param {Object} user - Объект с данными
+	 * @param {String} user.email - Почта пользователя
+	 * @param {String} user.surname - Фамилия пользователя
+	 * @param {String} user.name - Имя пользователя
+	 * @param {String} user.nickname - Псевдоним (никнейм) пользователя
+	 * @param {String} user.password - Пароль (никнейм) пользователя
+	 * @returns {Promise}
+	 */
 	signUp ( user ) {
 		return this.apiClient.post('register', { ...user });
 	},
