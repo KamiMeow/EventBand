a77e0f56acd6bf98673f58626cb0fef81dbe46ee
Дима Бурмистров <supamem74@gmail.com>
2020-06-03 17:23:21 +0300

    auth: unit test

---
+++ tests/unit/store/auth.spec.js
@@ -0,0 +1,49 @@
+import store from './store';
+
+const path = 'auth/';
+
+describe('Модуль AUTH', () => {
+	
+	it('Islogged - false', () => {
+		expect(store.getters[`${path}getIsLogged`]).toBe(false);
+	});
+
+	it('IsLoggedAsOrganization - false', () => {
+		expect(store.getters[`${path}getToken`]).toBe('');
+	});
+	
+	it('UUID Пользователя - Отсутствует', () => {
+		store.dispatch(`${path}logout`);
+		expect(store.getters[`${path}getUUID`]).toBe('');
+	});
+
+	it('Token Пользователя - Отсутствует', () => {
+		expect(store.getters[`${path}getToken`]).toBe('');
+	});
+
+	it('Token Организации пользователя - Отсутствует', () => {
+		expect(store.getters[`${path}getOrganizationToken`]).toBe('');
+	});
+
+	it('UUID Организации пользователя - Отсутствует', () => {
+		expect(store.getters[`${path}getOrganizationUuid`]).toBe('');
+	});	
+	
+	// ---------------------------------------
+	it('IsLogged - true', async () => {
+		await store.dispatch(`${path}signIn`, {
+			email: 'test@test.com',
+			password: 'test',
+		});
+		expect(store.getters[`${path}getIsLogged`]).toBeTruthy();
+	});
+
+	it('UUID Пользователя - получен', () => {
+		expect(store.getters[`${path}getUUID`].length).toBeGreaterThan(0);
+	});
+	
+	it('Token Пользователя - получен', () => {
+		expect(store.getters[`${path}getToken`].length).toBeGreaterThan(0);
+	});
+	
+});
