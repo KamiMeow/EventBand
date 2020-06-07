d88f7ce7d5adf33631970603ce566fc8ea401439
Дима Бурмистров <supamem74@gmail.com>
2020-06-07 17:43:23 +0300

    Organization layout

--- src/components/layout/OrganizationLayout/TheToolbar.vue
+++ src/components/layout/OrganizationLayout/TheToolbar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar 
-    color="secondary" 
+    color="primary" 
     clipped-left 
     dark 
     app
@@ -11,17 +11,68 @@
     <v-spacer />
 
     <v-toolbar-items>
-			
-    </v-toolbar-items>
+				<template v-for="item in toolbarItems">
+					<v-btn
+						v-if="item.needLogin === 'all' || item.needLogin === isLogged "
+						:key="item.link"
+						:to="item.link"
+						class="toolbar--items title text-none"
+						color="secondary"
+						text
+					>
+						{{ item.title }}
+					</v-btn>
+				</template>
+				
+				<new-organization-dialog/>
+				<v-btn
+					class="toolbar--items title text-none"
+					color="secondary"
+					text
+					@click="logoutAsOrganization"
+				>
+					To user profile
+				</v-btn>
+			</v-toolbar-items>
   </v-app-bar>
 </template>
 
 <script>
+import NewOrganizationDialog from './NewOrganizerDialog';
 /**
  * Компонент верхнего меню
  */
+const toolbarItems = [
+  { title: 'Organization',    link: '/my-organization/info', needLogin: true  },
+  { title: 'Create event',    link: '/my-organization/event/create', needLogin: true  },
+  { title: 'Create news',    link: '/my-organization/news/create', needLogin: true  },
+  { title: 'Edit info',    link: '/my-organization/edit', needLogin: true  },
+];
+
 
 export default {
-  name: "TheToolbar",
+	name: "TheToolbar",
+
+	components: {
+		NewOrganizationDialog,
+	},
+	
+	data: () => ({
+		toolbarItems,
+		drawer: false,
+	}),
+	
+	computed: {
+    isLogged() {
+      return this.$store.getters['auth/getIsLoggedAsOrganization'];
+    },
+	},
+	
+	methods: {
+		logoutAsOrganization() {			
+			this.$store.dispatch('auth/logoutAsOrganization');
+			this.$router.replace('/profile');
+		},
+	},
 };
 </script>
