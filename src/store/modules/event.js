858888dcb78afc584a8546ec9687ec74d5b63419
Дима Бурмистров <supamem74@gmail.com>
2020-06-07 12:27:20 +0300

    Store event: commented

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -2,29 +2,47 @@
 import services from '@/middleware';
 const { EventService } = services;
 
+/**
+ * Инициализирует изначальное состояние модуля
+ */
 export const initialState = () => ({
 	createdTickets: [],
 });
 
 
 export const mutations = {
+
+	/**
+	 * Добавляет новый билет в массив билетов мероприятия
+	 */
 	ADD_CREATED_TICKET: (state, ticket) => {
 		state.createdTickets.push(ticket);
 		console.log(ticket);
 	},
 
+	/**
+	 * Удаляет билет из массива билетов мероприятия
+	 */
 	REMOVE_TICKET: (state, ticket) => {
 		delete ticket.from;
 		delete ticket.to;
 		state.createdTickets.splice(state.createdTickets.indexOf(ticket), 1);
 	},
 	
+	/**
+	 * Очищает массив билетов 
+	 */
 	UNSET_EVENT_INFO: (state) => {
 		state.createdTickets.splice(0, state.createdTickets.length);
 	},
 };
 
 export const actions = {
+	/**
+	 * 
+	 * @param {Object} vuex 
+	 * @param {*} ticket 
+	 */
 	async createTicket({ commit, state }, ticket ) {
 		let res = (await EventService.createTicket(ticket)).data;
 		commit('ADD_CREATED_TICKET', res.ticket);
@@ -57,6 +75,9 @@ export const actions = {
 };
 
 export const getters = {
+	/**
+	 * Возвращает созданные билеты для мероприятия
+	 */
 	getCreatedTickets: state => state.createdTickets,
 };
 
